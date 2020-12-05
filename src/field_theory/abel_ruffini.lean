cc567444be7f4125d6a9c10e57d9f02c98185082
Thomas Browning <tb65536@uw.edu>
2020-12-04 17:08:03 -0800

    attempt at SBR

--- src/field_theory/abel_ruffini.lean
+++ src/field_theory/abel_ruffini.lean
@@ -208,3 +208,28 @@ begin
 end
 
 end rational_field
+
+section abel_ruffini
+
+universes u v
+
+variables (F : Type u) [field F] {E : Type v} [field E] [algebra F E]
+
+inductive SBR : E → Type (max u v)
+| base (a : F) : SBR (algebra_map F E a)
+--| add (a b : E) : SBR a → SBR b → SBR (a + b)
+--| neg (a : E) : SBR a → SBR (-a)
+--| mul (a b : E) : SBR a → SBR b → SBR (a * b)
+--| inv (a : E) : SBR a → SBR (a⁻¹)
+--| rad (a b : E) (n : ℕ) (h : a^n = b): SBR b → SBR a
+
+namespace SBR
+
+variables {F}
+
+def to_field : Π x : E, SBR F x → intermediate_field F E :=
+| (algebra_map F E x) (base x) := sorry
+
+end SBR
+
+end abel_ruffini
