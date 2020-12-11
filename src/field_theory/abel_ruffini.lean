5cb6068a9fe268040f33277438fbe96b2cb76dd4
Thomas Browning <tb65536@uw.edu>
2020-12-11 15:18:41 -0800

    progress

--- src/field_theory/abel_ruffini.lean
+++ src/field_theory/abel_ruffini.lean
@@ -10,24 +10,46 @@ open polynomial
 
 section abel_ruffini
 
-universes u v
+variables (F : Type*) [field F] {E : Type*} [field E] [algebra F E]
 
-variables (F : Type u) [field F] {E : Type v} [field E] [algebra F E]
-
-inductive SBR : E → Type (max u v)
+inductive SBR : E → Prop
 | base (a : F) : SBR (algebra_map F E a)
---| add (a b : E) : SBR a → SBR b → SBR (a + b)
---| neg (a : E) : SBR a → SBR (-a)
---| mul (a b : E) : SBR a → SBR b → SBR (a * b)
---| inv (a : E) : SBR a → SBR (a⁻¹)
---| rad (a b : E) (n : ℕ) (h : a^n = b): SBR b → SBR a
+| add (a b : E) : SBR a → SBR b → SBR (a + b)
+| neg (α : E) : SBR α → SBR (-α)
+| mul (α β : E) : SBR α → SBR β → SBR (α * β)
+| inv (α : E) : SBR α → SBR (α⁻¹)
+| rad (α : E) (n : ℕ) : SBR (α^n) → SBR α
 
 namespace SBR
 
-variables {F}
+variables {F} {α : E}
 
-def to_field : Π x : E, SBR F x → intermediate_field F E :=
-| (algebra_map F E x) (base x) := sorry
+theorem is_integral (h : SBR F α) : is_integral F α :=
+begin
+  revert h,
+  revert α,
+  apply SBR.rec,
+  { exact λ _, is_integral_algebra_map },
+  { exact λ _ _ _ _, is_integral_add },
+  { exact λ _ _, is_integral_neg },
+  { exact λ _ _ _ _, is_integral_mul },
+  { sorry },
+  { sorry },
+end
+
+theorem thm (h : SBR F α) : (is_solvable ((minimal_polynomial (is_integral h)).splitting_field
+  ≃ₐ[F] (minimal_polynomial (is_integral h)).splitting_field)) :=
+begin
+  apply @SBR.rec F _ E _ _ (λ a, ∀ h' : SBR F a,
+    (is_solvable ((minimal_polynomial (is_integral h')).splitting_field ≃ₐ[F]
+    (minimal_polynomial (is_integral h')).splitting_field))) _ _ _ _ _ _ _ h,
+  { sorry },
+  { sorry },
+  { sorry },
+  { sorry },
+  { sorry },
+  { sorry },
+end
 
 end SBR
 
