55b648c5599a76426210473b5adb9439f5662654
Thomas Browning <tb65536@uw.edu>
2020-12-23 11:59:53 -0800

    lemmas

--- src/group_theory/perm/generation.lean
+++ src/group_theory/perm/generation.lean
@@ -3,14 +3,22 @@ import group_theory.perm.cycles
 open equiv
 open equiv.perm
 
-lemma lem1 {α : Type*} [fintype α] :
+lemma closure_is_cycle {α : Type*} [fintype α] [linear_order α] :
   subgroup.closure ({σ | is_cycle σ} : set (perm α)) = ⊤ :=
 begin
-  sorry
+  rw eq_top_iff,
+  intros x hx,
+  obtain ⟨h1, h2, h3⟩ := subtype.mem (cycle_factors x),
+  rw ← h1,
+  exact subgroup.list_prod_mem _ (λ y hy, subgroup.subset_closure (h2 y hy)),
 end
 
-lemma lem2 {α : Type*} [fintype α] [decidable_eq α] :
+lemma closure_is_swap {α : Type*} [fintype α] [linear_order α] :
   subgroup.closure ({σ | is_swap σ} : set (perm α)) = ⊤ :=
 begin
-  sorry
+  rw eq_top_iff,
+  intros x hx,
+  obtain ⟨h1, h2⟩ := subtype.mem (swap_factors x),
+  rw ← h1,
+  exact subgroup.list_prod_mem _ (λ y hy, subgroup.subset_closure (h2 y hy)),
 end
