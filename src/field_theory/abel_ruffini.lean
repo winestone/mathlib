eeb31dede381c35c8274afd19a62868084d8479f
Thomas Browning <tb65536@uw.edu>
2020-12-31 16:59:19 -0800

    fleshing out the new approach

--- src/field_theory/abel_ruffini.lean
+++ src/field_theory/abel_ruffini.lean
@@ -6,7 +6,7 @@ import field_theory.algebraic_closure
 noncomputable theory
 open_locale classical
 
-open polynomial
+open polynomial intermediate_field
 
 section abel_ruffini
 
@@ -54,29 +54,22 @@ begin
   apply is_SBR.rec,
   { exact λ α, ⟨algebra_map F (SBR F E) α, rfl, base α⟩ },
   { intros α β hα hβ Pα Pβ,
-    obtain ⟨α₀, hα₀, Pα⟩ := Pα,
-    obtain ⟨β₀, hβ₀, Pβ⟩ := Pβ,
+    obtain ⟨⟨α₀, hα₀, Pα⟩, β₀, hβ₀, Pβ⟩ := ⟨Pα, Pβ⟩,
     exact ⟨α₀ + β₀, by {rw [←hα₀, ←hβ₀], refl }, add α₀ β₀ Pα Pβ⟩ },
   { intros α hα Pα,
     obtain ⟨α₀, hα₀, Pα⟩ := Pα,
     exact ⟨-α₀, by {rw ←hα₀, refl }, neg α₀ Pα⟩ },
   { intros α β hα hβ Pα Pβ,
-    obtain ⟨α₀, hα₀, Pα⟩ := Pα,
-    obtain ⟨β₀, hβ₀, Pβ⟩ := Pβ,
+    obtain ⟨⟨α₀, hα₀, Pα⟩, β₀, hβ₀, Pβ⟩ := ⟨Pα, Pβ⟩,
     exact ⟨α₀ * β₀, by {rw [←hα₀, ←hβ₀], refl }, mul α₀ β₀ Pα Pβ⟩ },
   { intros α hα Pα,
     obtain ⟨α₀, hα₀, Pα⟩ := Pα,
     exact ⟨α₀⁻¹, by {rw ←hα₀, refl }, inv α₀ Pα⟩ },
   { intros α n hn hα Pα,
     obtain ⟨α₀, hα₀, Pα⟩ := Pα,
-    use ⟨α, is_SBR.rad α n hn hα⟩,
-    split,
-    { refl },
-    { refine rad _ n hn _,
-      convert Pα,
-      ext,
-      rw hα₀,
-      exact (SBR F E).coe_pow _ n } }
+    refine ⟨⟨α, is_SBR.rad α n hn hα⟩, rfl, rad _ n hn _⟩,
+    convert Pα,
+    exact subtype.ext (eq.trans ((SBR F E).coe_pow _ n) hα₀.symm) }
 end
 
 theorem is_integral (α : SBR F E) : is_integral F α :=
@@ -87,40 +80,63 @@ begin
   { exact λ _ _, is_integral_add },
   { exact λ _, is_integral_neg },
   { exact λ _ _, is_integral_mul },
-  { intros α hα,
-    have : is_algebraic F (↑(⟨α, hα⟩ : integral_closure F (SBR F E)) : (SBR F E)),
-    { exact (is_algebraic_iff_is_integral F).mpr hα, },
-    exact subalgebra.inv_mem_of_algebraic (integral_closure F (SBR F E)) this },
+  { exact λ α hα, subalgebra.inv_mem_of_algebraic (integral_closure F (SBR F E))
+      (show is_algebraic F ↑(⟨α, hα⟩ : integral_closure F (SBR F E)),
+        by exact (is_algebraic_iff_is_integral F).mpr hα) },
   { intros α n hn hα,
-    rw ← is_algebraic_iff_is_integral,
     obtain ⟨p, h1, h2⟩ := (is_algebraic_iff_is_integral F).mpr hα,
-    refine ⟨p.comp (X ^ n), ⟨_, by rw [aeval_comp, aeval_X_pow, h2]⟩⟩,
-    intro h,
-    rw [←leading_coeff_eq_zero, leading_coeff_comp, leading_coeff_X_pow, one_pow, mul_one] at h,
-    exact h1 (leading_coeff_eq_zero.mp h),
+    refine (is_algebraic_iff_is_integral F).mp ⟨p.comp (X ^ n),
+      ⟨λ h, h1 (leading_coeff_eq_zero.mp _), by rw [aeval_comp, aeval_X_pow, h2]⟩⟩,
+    rwa [←leading_coeff_eq_zero, leading_coeff_comp, leading_coeff_X_pow, one_pow, mul_one] at h,
     rwa nat_degree_X_pow }
 end
 
-theorem thm (α : SBR F E) : (is_solvable ((minimal_polynomial (is_integral α)).splitting_field
-  ≃ₐ[F] (minimal_polynomial (is_integral α)).splitting_field)) :=
+def P (α : SBR F E) : Prop :=
+  is_solvable ((minimal_polynomial (is_integral α)).splitting_field ≃ₐ[F]
+    (minimal_polynomial (is_integral α)).splitting_field)
+
+lemma induction0 (α : F) : P (algebra_map F (SBR F E) α) :=
+begin
+  rw [P, minimal_polynomial.eq_X_sub_C],
+  let K := (X - C α).splitting_field,
+  have h1 : ⊤ = ⊥ := (is_splitting_field.splits_iff K (X - C α)).mp
+    (splits_X_sub_C (ring_hom.id F)),
+  have h2 : K ≃ₐ[F] F := (algebra.top_equiv.symm.trans
+    (intermediate_field.subalgebra.equiv_of_eq h1)).trans (algebra.bot_equiv F K),
+  have h3 : (K ≃ₐ[F] K) ≃* (F ≃ₐ[F] F),
+  { sorry },
+  sorry,
+end
+
+lemma induction1 {α β : SBR F E} (hγ : ↑β ∈ F⟮(α : E)⟯) (hα : P α) : P β :=
+begin
+  sorry,
+end
+
+lemma induction2 {α β γ : SBR F E} (hγ : ↑γ ∈ F⟮(α : E), (β : E)⟯) (hα : P α) (hβ : P β) : P γ :=
+begin
+  sorry,
+end
+
+lemma induction3 : "nth root stuff" = "seems pretty tricky" := sorry
+
+theorem thm (α : SBR F E) : P α :=
 begin
   revert α,
   apply SBR.induction,
-  { intro α,
-    rw minimal_polynomial.eq_X_sub_C,
-    let K := (X - C α).splitting_field,
-    have h1 : ⊤ = ⊥ := (is_splitting_field.splits_iff K (X - C α)).mp
-      (splits_X_sub_C (ring_hom.id F)),
-    have h2 : K ≃ₐ[F] F := (algebra.top_equiv.symm.trans
-      (intermediate_field.subalgebra.equiv_of_eq h1)).trans (algebra.bot_equiv F K),
-    have h3 : (K ≃ₐ[F] K) ≃* (F ≃ₐ[F] F),
-    { sorry },
-    sorry, },
+  { exact induction0 },
   { intros α β,
+    apply induction2,
+    change ↑α + ↑β ∈ _,
     sorry },
-  { sorry },
-  { sorry },
-  { sorry },
+  { exact λ α, induction1 (neg_mem _ (mem_adjoin_simple_self F (α : E))) },
+  { intros α β,
+    apply induction2,
+    change ↑α * ↑β ∈ _,
+    apply mul_mem,/- Maybe add these two lemmas to adjoin.lean ? -/
+    sorry,
+    sorry },
+  { exact λ α, induction1 (inv_mem _ (mem_adjoin_simple_self F (α : E))) },
   { sorry },
 end
 
