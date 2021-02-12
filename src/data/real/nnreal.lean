e606715a093a6e315229aade3e5401d4b54ff0d4
RemyDegenne <Remydegenne@gmail.com>
2021-02-12 13:03:39 +0100

    remove changes to nnreal.lean

--- src/data/real/nnreal.lean
+++ src/data/real/nnreal.lean
@@ -228,15 +228,13 @@ instance : canonically_linear_ordered_add_monoid ℝ≥0 :=
   ..nnreal.order_bot,
   ..nnreal.linear_order }
 
-instance : canonically_linear_ordered_add_monoid ℝ≥0 :=
-{ .. nnreal.canonically_ordered_add_monoid, .. nnreal.linear_order}
-
 instance : distrib_lattice ℝ≥0 := by apply_instance
 
 instance : semilattice_inf_bot ℝ≥0 :=
 { .. nnreal.order_bot, .. nnreal.distrib_lattice }
 
-instance : semilattice_sup_bot ℝ≥0 := canonically_linear_ordered_add_monoid.semilattice_sup_bot
+instance : semilattice_sup_bot ℝ≥0 :=
+{ .. nnreal.order_bot, .. nnreal.distrib_lattice }
 
 instance : linear_ordered_semiring ℝ≥0 :=
 { add_left_cancel            := assume a b c h, nnreal.eq $
