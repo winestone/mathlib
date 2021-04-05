32d50dcd831e5d7def427177aeef72458653bdc9
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2021-04-05 21:12:41 +0200

    feat(data/multiset/basic): add nsmul_cons

--- src/data/multiset/basic.lean
+++ src/data/multiset/basic.lean
@@ -423,6 +423,13 @@ begin
     exact or.inl h }
 end
 
+lemma nsmul_cons {s : multiset α} (n : ℕ) (a : α) : n • (a ::ₘ s) = n • (a ::ₘ 0) + n • s :=
+begin
+  induction n with n ih,
+  { simp only [add_zero, zero_nsmul] },
+  { simp only [succ_nsmul, one_nsmul, ih, nsmul_add, add_cons, ←singleton_add a s] }
+end
+
 /-! ### Cardinality -/
 
 /-- The cardinality of a multiset is the sum of the multiplicities
