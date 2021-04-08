1192546a20713f8e30f58f4bb65246aec40b45d5
RemyDegenne <Remydegenne@gmail.com>
2021-04-08 15:11:45 +0200

    change tendsto_Lp_iff_tendsto_lp

--- src/measure_theory/lp_space.lean
+++ src/measure_theory/lp_space.lean
@@ -1539,33 +1539,21 @@ end
 
 /-! ### `Lp` is complete iff Cauchy sequences of `ℒp` have limits in `ℒp` -/
 
-lemma tendsto_Lp_of_tendsto_ℒp {ι} [linear_order ι] [hp : fact (1 ≤ p)]
-  {f : ι → Lp E p μ} (f_lim : α → E) (f_lim_ℒp : mem_ℒp f_lim p μ)
-  (h_tendsto : at_top.tendsto (λ n, snorm (f n - f_lim) p μ) (𝓝 0)) :
-  at_top.tendsto f (𝓝 (f_lim_ℒp.to_Lp f_lim)) :=
+lemma tendsto_Lp_iff_tendsto_ℒp' {ι} [linear_order ι] [hp : fact (1 ≤ p)]
+  (f : ι → Lp E p μ) (f_lim : Lp E p μ) :
+  at_top.tendsto f (𝓝 f_lim) ↔ at_top.tendsto (λ n, snorm (f n - f_lim) p μ) (𝓝 0) :=
 begin
-  by_cases hι : nonempty ι,
-  swap, { exact tendsto_of_not_nonempty hι, },
-  haveI : nonempty ι := hι,
-  rw ennreal.tendsto_at_top_zero at h_tendsto,
-  simp_rw metric.tendsto_at_top,
-  intros ε hε,
-  have hε_pos : 0 < ennreal.of_real ε, from ennreal.of_real_pos.mpr hε,
-  have hε2_pos : 0 < ennreal.of_real ε / 2,
-    by { rw ennreal.div_pos_iff, exact ⟨hε_pos.ne.symm, ennreal.two_ne_top⟩, },
-  cases (h_tendsto (ennreal.of_real ε / 2) hε2_pos) with N h_tendsto,
-  refine ⟨N, λ n hn, _⟩,
-  specialize h_tendsto n hn,
-  have hp_pos : 0 < p, from ennreal.zero_lt_one.trans_le hp.elim,
-  rw [dist_def, ←@ennreal.to_real_of_real ε (le_of_lt hε),
-    ennreal.to_real_lt_to_real _ ennreal.of_real_ne_top],
-  swap, { rw snorm_congr_ae (coe_fn_sub _ _).symm, exact snorm_ne_top _, },
-  have h_coe : ⇑(f n) - f_lim =ᵐ[μ] ⇑(f n) - ⇑(mem_ℒp.to_Lp f_lim f_lim_ℒp),
-  { have h_coe' : f_lim =ᵐ[μ] ⇑(mem_ℒp.to_Lp f_lim f_lim_ℒp), from (mem_ℒp.coe_fn_to_Lp _).symm,
-    refine h_coe'.mono (λ x hx, _),
-    rw [pi.sub_apply, pi.sub_apply, hx], },
-  rw snorm_congr_ae h_coe.symm,
-  exact lt_of_le_of_lt h_tendsto (ennreal.half_lt_self hε_pos.ne.symm ennreal.of_real_ne_top),
+  rw [tendsto_iff_dist_tendsto_zero, ← ennreal.zero_to_real],
+  simp_rw dist_def,
+  refine ⟨λ h, _, λ h, tendsto.comp (ennreal.tendsto_to_real ennreal.coe_ne_top) h⟩,
+  have h_snorm_eq : (λ n, snorm (⇑(f n) - ⇑f_lim) p μ)
+    = (λ n, ennreal.of_real (snorm (⇑(f n) - ⇑f_lim) p μ).to_real),
+  { ext1 n,
+    rw ennreal.of_real_to_real,
+    rw snorm_congr_ae (Lp.coe_fn_sub _ _).symm,
+    exact snorm_ne_top _,  },
+  rw [h_snorm_eq, ← @ennreal.of_real_to_real 0 ennreal.coe_ne_top],
+  exact ennreal.tendsto_of_real h,
 end
 
 lemma tendsto_Lp_iff_tendsto_ℒp {ι} [linear_order ι] [hp : fact (1 ≤ p)]
@@ -1573,37 +1561,19 @@ lemma tendsto_Lp_iff_tendsto_ℒp {ι} [linear_order ι] [hp : fact (1 ≤ p)]
   at_top.tendsto f (𝓝 (f_lim_ℒp.to_Lp f_lim))
     ↔ at_top.tendsto (λ n, snorm (f n - f_lim) p μ) (𝓝 0) :=
 begin
-  refine ⟨λ h_tendsto, _, tendsto_Lp_of_tendsto_ℒp f_lim f_lim_ℒp⟩,
-  by_cases hι : nonempty ι,
-  swap, { exact tendsto_of_not_nonempty hι, },
-  haveI : nonempty ι := hι,
-  rw ennreal.tendsto_at_top_zero,
-  rw metric.tendsto_at_top at h_tendsto,
-  refine λ ε hε, _,
-  by_cases hε_top : ε = ∞,
-  { simp_rw hε_top, exact ⟨hι.some, λ n hn, le_top⟩, },
-  have hε_pos : 0 < ε.to_real, from ennreal.to_real_pos_iff.mpr ⟨hε, hε_top⟩,
-  cases (h_tendsto ε.to_real hε_pos) with N h_tendsto,
-  refine ⟨N, λ n hn, _⟩,
-  specialize h_tendsto n hn,
-  rw [dist_def, ennreal.to_real_lt_to_real _ hε_top] at h_tendsto,
-  swap, { rw [←lt_top_iff_ne_top, snorm_congr_ae (Lp.coe_fn_sub _ _).symm],
-    exact Lp.snorm_lt_top _, },
-  have h_eq : snorm (⇑(f n) - ⇑(mem_ℒp.to_Lp f_lim f_lim_ℒp)) p μ = snorm (⇑(f n) - f_lim) p μ,
-  { have h_coe_fn : ⇑(mem_ℒp.to_Lp f_lim f_lim_ℒp) =ᵐ[μ] f_lim, from f_lim_ℒp.coe_fn_to_Lp,
-    exact snorm_congr_ae (eventually_eq.sub eventually_eq.rfl h_coe_fn), },
-  rw h_eq at h_tendsto,
-  exact h_tendsto.le,
+  rw tendsto_Lp_iff_tendsto_ℒp',
+  suffices h_eq : (λ n, snorm (⇑(f n) - ⇑(mem_ℒp.to_Lp f_lim f_lim_ℒp)) p μ)
+      = (λ n, snorm (⇑(f n) - f_lim) p μ),
+    by rw h_eq,
+  ext1 n,
+  exact snorm_congr_ae (eventually_eq.sub eventually_eq.rfl (mem_ℒp.coe_fn_to_Lp f_lim_ℒp)),
 end
 
-lemma tendsto_Lp_iff_tendsto_ℒp' {ι} [linear_order ι] [hp : fact (1 ≤ p)]
-  (f : ι → Lp E p μ) (f_lim : Lp E p μ) :
-  at_top.tendsto f (𝓝 f_lim) ↔ at_top.tendsto (λ n, snorm (f n - f_lim) p μ) (𝓝 0) :=
-begin
-  rw tendsto_iff_dist_tendsto_zero,
-  simp_rw dist_def,
-  sorry
-end
+lemma tendsto_Lp_of_tendsto_ℒp {ι} [linear_order ι] [hp : fact (1 ≤ p)]
+  {f : ι → Lp E p μ} (f_lim : α → E) (f_lim_ℒp : mem_ℒp f_lim p μ)
+  (h_tendsto : at_top.tendsto (λ n, snorm (f n - f_lim) p μ) (𝓝 0)) :
+  at_top.tendsto f (𝓝 (f_lim_ℒp.to_Lp f_lim)) :=
+by rwa tendsto_Lp_iff_tendsto_ℒp f f_lim f_lim_ℒp
 
 lemma complete_space_Lp_of_cauchy_complete_ℒp [hp : fact (1 ≤ p)]
   (H : ∀ (f : ℕ → α → E) (hf : ∀ n, mem_ℒp (f n) p μ) (B : ℕ → ℝ≥0∞) (hB : ∑' i, B i < ∞)
